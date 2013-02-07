5a8a3383fe6b63189e131824eda9487d986be1cb
mariozig <mariozig@gmail.com>
2013-02-07 09:30:07 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OlfactoryFactory::Application.config.secret_token = '5264f961c4fb94a29695320ef5311d178c408c59f2c0f7680727e631d76ff6e36c6fdfb174e1472c6680938de1aca8ad44ff1e2a641b417bc3134b313af9b1a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OlfactoryFactory::Application.config.session_store :cookie_store, key: '_olfactory_factory_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OlfactoryFactory::Application.config.session_store :active_record_store
